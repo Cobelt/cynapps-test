98afa6ebb2e724e9156a14e96ebc756249bcba09
Cobelt <paul_emile@hotmail.fr>
2024-04-22 22:55:07 +0200

    feat: removed default page and added custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      light: "#fff9ef",
+      pastelpink: "#ffd5dc",
+      hotpink: "#FFAEBC",
+      darkpink: "#955a63",
+      darkerpink: "#441b21",
+      tiffanyblue: "#A0E7E5",
+      mint: "#B4F8C8",
+      cream: "#FBE7C6",
+    },
     extend: {},
   },
   plugins: [],
